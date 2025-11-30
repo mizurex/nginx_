dfa1e6023234991d9b0ff332f4eef46af5628e51
mizurex <viswasdhal@gmail.com>
2025-11-30 23:49:48 +0530

    added worker

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-## Very basic API architecture
+## Very basic architecture concepts
 
 Horizontal Scaling (Multiple App Instances)
 Load Balancing (Nginx)
 Distributed Rate Limiting
 Containerization (Docker)
+Redis queues
+Workers
 
 ```
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 import express from "express"
 import { rate_limit } from "./rate-limit/rate-limit.js";
 import v1Router from "./routes/v1/index.js";
+import { createClient } from "redis";
 
 const app = express();
 const PORT = 3000;
 
 const dup = process.env.APP_NAME;
 
+const redisClient = createClient({
+  url: process.env.REDIS_URL || "redis://localhost:6379"
+});
+redisClient.connect();
+console.log("Connected to Redis");
 
 app.set('trust proxy', true);
 
@@ -14,13 +20,6 @@ app.set('trust proxy', true);
 app.use(express.json());
 app.use(rate_limit);
 
-app.get('/health', (req, res) => {
-  res.status(200).json({ 
-    status: 'healthy', 
-    app: dup,
-    timestamp: new Date().toISOString()
-  });
-});
 
 app.get('/', (req, res) => {
   res.json({ 
@@ -37,8 +36,26 @@ app.get('/', (req, res) => {
   console.log(`request served by ${dup}`)
 });
 
-app.use('/api/v1', v1Router);
+//add data to redis
+app.post('/api/v1/data',async (req, res) => {
+  const {username, email, password} = req.body;
 
+  try{
+   await redisClient.lpush("data", JSON.stringify({username, email, password}));
+    res.status(200).json({ message: "Data added to Redis queue", data: JSON.stringify({username, email, password}) });
+  }
+  catch(error){
+    console.error("Error adding data to queue", error);
+    res.status(500).json({ message: "Error adding data to queue", error: error.message });
+  }
+  
+ 
+});
+
+
+
+
+app.use('/api/v1', v1Router);
 app.use((req, res) => {
   res.status(404).json({ 
     error: 'Not found',
